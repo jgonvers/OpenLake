ca992cde342fb390885f434d4923cf24e1556865
Joachim Gonvers <joachim.gonvers@gmail.com>
2021-03-29 16:50:18 +0200

    removes image_link from category

--- db/migrate/20210329102226_create_categories.rb
+++ db/migrate/20210329102226_create_categories.rb
@@ -2,8 +2,7 @@ class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
       t.string :name
-      t.string :logo_link, default: "category/icon/default.svg"
-      t.string :photo_link, default: "category/image/default.jpg"
+      t.string :logo_link, default: "category/default.svg"
     end
   end
 end
