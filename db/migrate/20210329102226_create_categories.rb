28085fe6da5efa8c11181968fde8dd4c521ff302
Joachim Gonvers <joachim.gonvers@gmail.com>
2021-03-29 16:35:06 +0200

    default value for category image

--- db/migrate/20210329102226_create_categories.rb
+++ db/migrate/20210329102226_create_categories.rb
@@ -2,8 +2,8 @@ class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
       t.string :name
-      t.string :logo_link
-      t.string :photo_link
+      t.string :logo_link, default: "category/icon/default.svg"
+      t.string :photo_link, default: "category/image/default.jpg"
     end
   end
 end
