1dd54730df22f8acef393c6df1a34d6b36784088
Kilian Hayat <khayat@citycable.ch>
2021-03-29 17:57:38 +0200

    create validation for events and categories model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+  validates :name, :logo_link, presence: true
+  validates :name, acceptance: { accept: ['football', 'rugby', 'running', 'swimming', 'handball', 'volleyball'] }
   has_many :events
 end
--- db/migrate/20210329153648_change_default_value_teammate_link_status.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDefaultValueTeammateLinkStatus < ActiveRecord::Migration[6.0]
-  def change
-    change_column :teammate_links, :status, :string, default: 'pending'
-  end
-end
