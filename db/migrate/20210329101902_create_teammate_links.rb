566d7030e3da94e459d85d02e659155a3906d27e
Joachim Gonvers <joachim.gonvers@gmail.com>
2021-03-29 14:09:38 +0200

    added default value to teammate_link

--- db/migrate/20210329101902_create_teammate_links.rb
+++ db/migrate/20210329101902_create_teammate_links.rb
@@ -2,7 +2,7 @@ class CreateTeammateLinks < ActiveRecord::Migration[6.0]
   def change
     create_table :teammate_links do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :status
+      t.string :status, default: "pending"
       t.references :teammate, null: false, foreign_key: {to_table: :users}
     end
   end
