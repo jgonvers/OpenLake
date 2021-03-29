1355afb47f01c47ff9ae1aa7cd6d5c282180e604
Kilian Hayat <khayat@citycable.ch>
2021-03-29 17:46:37 +0200

    create validation for users and reviews  model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
+  validates :title, :address, :datetime, :status, :content, :participants_maximum, presence: true
+
   has_many :reviews
   has_many :attendances
   has_many :users, through: :attendances
