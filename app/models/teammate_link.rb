bc00a826e712ccbcd66b46313199a9c86442f1e0
Joachim Gonvers <joachim.gonvers@gmail.com>
2021-03-30 10:55:10 +0200

    corrected associtations, added teammate_common

--- app/models/teammate_link.rb
+++ app/models/teammate_link.rb
@@ -1,5 +1,5 @@
 class TeammateLink < ApplicationRecord
   validates :status, acceptance: { accept: ['accepted', 'pending', 'declined'] }
   belongs_to :user
-  belongs_to :teammate
+  belongs_to :teammate, class_name: :User
 end
