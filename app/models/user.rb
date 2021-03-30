b3e2bff3c750bb553f715d69c8394e5a3c0992c1
Kilian Hayat <khayat@citycable.ch>
2021-03-30 15:13:10 +0200

    converted the address to longitude and latitude

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   has_many :teammates, through: :teammate_link
   has_many :created_events, foreign_key: :creator_id, class_name: :Event
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   def common_teammates
     count = 0
     teammates.each do |teammate|
