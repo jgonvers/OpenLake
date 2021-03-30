19205d7c36aba7b4c6b2aaa976ba851c6236ba86
Kilian Hayat <khayat@citycable.ch>
2021-03-30 17:49:25 +0200

    add the map on the shows 2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,3 +32,13 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
+
+// map
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+document.addEventListener('turbolinks:load', () => {
+  initMapbox();
+});
--- app/models/event.rb
+++ app/models/event.rb
@@ -9,4 +9,5 @@ class Event < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
 end
