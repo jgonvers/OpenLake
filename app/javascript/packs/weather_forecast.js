48b88acc66503f929c0987ba8c85df21527d6384
Kilian Hayat <khayat@citycable.ch>
2021-03-31 11:32:11 +0200

    add icon weather on the show

--- app/javascript/packs/weather_forecast.js
+++ app/javascript/packs/weather_forecast.js
@@ -1,29 +1,24 @@
-const addressUser = document.querySelector('#address');
-const btnSend = document.querySelector('#btnsend');
-const divDisplay = document.querySelector('#display-weather');
+
+const divDisplay = document.querySelector('#weather-fc');
 
 
-const requestAPI = (cityValue) => {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=b36426291fcbb98ff7c07c74bc4b7990`)
+const requestAPI = (cityValue, dateevent) => {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue},${dateevent}&appid=b36426291fcbb98ff7c07c74bc4b7990`)
     .then(response => response.json())
     .then((data) => {
       console.log(data);
+      console.log(divDisplay);
       const temperature = Math.round((data.main.temp - 273.15) * 100) / 100;
       const cloud = data.weather[0].description;
       console.log(temperature); // Get the temp for the city
-      const today = new Date();
-      const localOffset = data.timezone + today.getTimezoneOffset() * 60;
-      const localDate = new Date(today.setUTCSeconds(localOffset));
-      const options = {
-        weekday: 'long', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'
-      };
-      const formattedDate = localDate.toLocaleDateString("en-US", options);
-      // divDisplay.insertAdjacentHTML("beforeend", `<h2>Weather in ${cityValue}</h2>`);
-      // divDisplay.insertAdjacentHTML("beforeend", `<p><strong>${formattedDate}</strong></p>`);
-      // divDisplay.insertAdjacentHTML("beforeend", `<p>${cloud}</p>`);
-      // divDisplay.insertAdjacentHTML("beforeend", `<h4><strong>${temperature}°</strong> Celsius Degree</h4>`);
+      icon_weather = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`; // please let this line even if unuseful
+      console.log(icon_weather)
+      divDisplay.insertAdjacentHTML("beforeend", `<h3><strong>${temperature}°</strong></h3>`);
+      divDisplay.insertAdjacentHTML("beforeend", `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" style="height: 30px; width: 30px;"> `);
+      divDisplay.insertAdjacentHTML("beforeend", `<h3><strong>${cloud}°</strong></h3>`);
+
     });
 };
 
-console.log(requestAPI("lausanne"));
+console.log(requestAPI("lausanne", "Tue, 30 Mar 2021 13:31:15 UTC +00:00"));
 
