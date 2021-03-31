d42cd0c1d883b6633c7cc324fca260d27b163de0
Joachim Gonvers <joachim.gonvers@gmail.com>
2021-03-31 08:58:08 +0200

    seed without avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,33 +7,41 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+
+def teammate_create(user1, user2)
+  if TeammateLink.where(user:user1, teammate:user2).count.zero?
+    TeammateLink.new(user:user1, teammate:user2).save!
+    TeammateLink.new(user:user2, teammate:user1).save!
+  end
+end
+
 address_list = ["Lausanne", "Morges", "Renens", "Montreux", "Moudon", "Genève"]
 
 lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
+  category = {
+    bowling: '<i class="fas fa-bowling-ball" style="color: #ce4e4e; font-size: 15px;"></i>',
+    biking: '<i class="fas fa-biking" style="color: #32b53b; font-size: 15px;"></i>',
+    football: '<i class="fas fa-futbol" style="color: #735BBF; font-size: 15px;"></i>',
+    running: '<i class="fas fa-running" style="color: orange; font-size: 15px;"></i>',
+    swimming: '<i class="fas fa-swimmer" style="color: #59cdea; font-size: 15px;"></i>',
+    volleyball: '<i class="fas fa-volleyball-ball" style="color: #FFD700; font-size: 15px;"></i>'
+  }
+  
+
+
 
 puts "create 1 user user@email.com"
-
 u = User.new(
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
-  content: lorem
+  content: lorem,
   password: '1234567',
   password_confirmation: '1234567',
   email: "user@email.com",
   address: address_list.sample
   )
 u.save!
-
-category = {
-  Bowling: '<i class="fas fa-bowling-ball" style="color: #ce4e4e; font-size: 15px;"></i>',
-  Biking: '<i class="fas fa-biking" style="color: #32b53b; font-size: 15px;"></i>',
-  Football: '<i class="fas fa-futbol" style="color: #735BBF; font-size: 15px;"></i>',
-  Running: '<i class="fas fa-running" style="color: orange; font-size: 15px;"></i>',
-  Swimming: '<i class="fas fa-swimmer" style="color: #59cdea; font-size: 15px;"></i>',
-  Volleyball: '<i class="fas fa-volleyball-ball" style="color: #FFD700; font-size: 15px;"></i>'
-}
-
 
 puts "create categories"
 category.each do |cat, val|
@@ -44,7 +52,7 @@ category.each do |cat, val|
 end
 
 puts "create 20 user"
-10.times do
+20.times do
   fname = Faker::Name.first_name
   lname = Faker::Name.last_name
   un = User.new(
@@ -53,9 +61,10 @@ puts "create 20 user"
     content: lorem,
     password: '1234567',
     password_confirmation: '1234567',
-    email: "#{fname.downcase}.#{lname.downcase}@email.com"
+    email: "#{fname.downcase}.#{lname.downcase}@email.com",
     address: address_list.sample
-  )
+  ).save!
+  puts "."
 end
 
 puts "create an event for user@email.com"
@@ -65,7 +74,7 @@ e = Event.new(
   address: "Lausanne",
   creator: u,
   content: lorem,
-  category: category.where(name:"Footbal").first,
+  category: Category.where(name:"football").first,
   start_time: date,
   end_time: date + 3600,
   participants_maximum: 20
@@ -75,10 +84,59 @@ e.save!
 puts "add 10 attendant to event"
 10.times do
   u2 = User.all.sample
-  if u2 != u
+  if u2 != u && !(e.users.include? u2)
     Attendance.new(
       user: u2,
       event: e
-    )
+    ).save!
   end
+  puts "."
+end
+
+
+puts "create 5 other event with 5 attendant"
+n=1
+5.times do
+  c = Category.all.sample
+  u2 = User.all.sample
+  e = Event.new(
+    title: "#{Faker::Adjective.positive} #{c.name.downcase}",
+    address: address_list.sample,
+    creator: u,
+    content: lorem,
+    category: c,
+    start_time: date + n*60*60*24,
+    end_time: date + 3600 + n*60*60*24,
+    participants_maximum: 20
+  )
+  e.save!
+  5.times do
+    u2 = User.all.sample
+    if u2 != u && !(e.users.include? u2)
+      Attendance.new(
+        user: u2,
+        event: e
+      ).save!
+    end
+  end
+  puts "."
+end
+
+puts "add up to 10 teammates to user@email.com"
+u = User.first
+10.times do 
+  u2 = User.all.sample
+  teammate_create(u, u2)
+  puts "."
+end
+
+puts "create 50 teammates links"
+50.times do
+  u = User.all.sample
+  u2 = u
+  while u == u2
+    u2 = User.all.sample
+  end
+  teammate_create(u, u2)
+  puts "."
 end
