0c44a077bef52be146c3fbe514bd0002176f1946
Kilian Hayat <khayat@citycable.ch>
2021-04-01 15:33:25 +0200

    create the back end for create en event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -21,11 +21,30 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(
+      category_id: event_params[:category], title: event_params[:title],
+      content: event_params[:content], address: event_params[:address],
+      participants_maximum: event_params[:participants_maximum],
+      start_time: event_params[:start_time],
+      end_time: event_params[:end_time]
+    )
+    @event.creator_id = current_user.id
+    if @event.save!
+      redirect_to events_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:category, :title, :content, :address, :participants_maximum, :start_time, :end_time)
   end
 end
