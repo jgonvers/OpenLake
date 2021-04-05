23c4ff451ff22427768ed27e37a32abcc8de219b
maxime jost <maxime.jost@gmail.com>
2021-04-05 10:28:24 +0200

    added autocomplete to address in create an event

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,10 +28,10 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
+//document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-});
+//});
 
 // map
 
@@ -39,6 +39,9 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { initMapbox } from '../plugins/init_mapbox';
 
+import { initAutocomplete } from '../plugins/init_autocomplete';
+
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+  initAutocomplete();
 });
